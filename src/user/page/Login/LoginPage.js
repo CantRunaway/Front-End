3d4bafd86b3147f64b2fe7f25c58f840b9319ef3
hj-8888 <xcxc4189@naver.com>
2022-10-11 22:43:45 +0900

    회원가입 요청 프로트 완료
    회원가입 요청 프로트 완료

--- src/user/page/Login/LoginPage.js
+++ src/user/page/Login/LoginPage.js
@@ -1,18 +1,20 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import './css/LoginPage.css'
 
 function LoginPage() {
   return (
-    <Fragment>
-      <div className='login-main'>
+      <div className='login'>
+
         <div className='login-logo'>
           <img className="main-icon" alt="main-icon" src="img/ICON.jpg" />
         </div>
+
         <div className='login-banner'>
           <span>도망못가</span>
           <span>당신의 출퇴근 관리를 하나로.</span>
         </div>
-        <div className='login-form-div'>
+
+        <div className='login-form-box'>
           <form className='login-form'>
               <div className='login'>
                 <input placeholder='ID' className='id'></input>
@@ -21,11 +23,12 @@ function LoginPage() {
                 <button className='login-button'>Login</button>
             </form>
         </div>
+
         <div>
           <button className='login-sign-up'>회원가입</button>
         </div>
+
       </div>
-    </Fragment>
   )
 }
 
--- src/worker/page/UserInfo/UserInfoPage.js
+++ src/worker/page/UserInfo/UserInfoPage.js
@@ -1,24 +1,89 @@
 import React from 'react'
 import '../UserInfo/css/UserInfoPage.css'
-import Header from '../../../etc/components/Header'
-import SideMenu from '../../../etc/components/SideMenu'
-import UserInfoSession from '../UserInfo/components/UserInfoSession'
 
 function UserInfoPage() {
   return (
-    <div className='UserInfoPage'>
-    {/* 상단 */}
-      <header>
-        <Header/>
-      </header>
-      {/* 사이드 메뉴 */}
-      <div className='SideMenu'>
-        <SideMenu/>
+    <div className='userInfo'>
+
+      <div className='login-logo'>
+        <img className="main-icon" alt="main-icon" src="/img/ICON.jpg" />
       </div>
-      {/* 사용자 정보 조회 영역 */}
-      <div className='UserInfoSession'>
-        <UserInfoSession/>
+      
+      <div className='userInfo-banner'>
+        <span>도망못가</span>
+        <span>-- 회원정보 --</span>
       </div>
+
+      <div className='userInfo-main'>
+
+        <span className='name'>
+          이름 <input className='name-input' />
+        </span>
+
+        <span className='student-code'>
+          학번 <input className='student-code-input' />
+        </span>
+
+        <span className='pw'>
+          비밀번호 <input className='pw-input' />
+        </span>
+
+        <span className='select-grade'>
+          학년
+          <select className='grade-list'>
+            <option value='none' disabled>--선택--</option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+          </select >
+        </span>
+
+        <span className='select-department'>
+          학과
+          <select className='department-list'>
+            <option value='none' disabled>--선택--</option>
+            <option value='1'>컴소공</option>
+            <option value='2'>컴터</option>
+            <option value='3'>전자</option>
+            <option value='4'>기계</option>
+          </select>
+        </span>
+
+        <span className='phone'>
+          전화번호 <input className='phone'/>
+        </span>
+
+        <span className='birth'>
+          생년월일 <input className='birth-input' />
+        </span>
+      
+
+        <span className='account'>
+          지급 계좌 
+          <select className='bank-list'>
+            <option value='none' disabled>--선택--</option>
+            <option value='1'>국민</option>
+            <option value='2'>농협</option>
+          </select>
+
+          <input className='account-input'/>
+        </span>
+
+        <span className='work-type'>
+          근무 종류
+          <select className='work-type-list'>
+            <option value='none' disabled>--선택--</option>
+            <option value='1'>식기 세척</option>
+            <option value='2'>식사 확인</option>
+          </select>
+        </span>
+
+      </div>
+      
+      <button className='sign-up-button'>가입 요청</button>
+
     </div>
   )
 }
