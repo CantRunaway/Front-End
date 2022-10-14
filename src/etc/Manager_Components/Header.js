710082c058f3bf7dd9b1c85d52ea02bfafcdcee0
so0y <jenny1717@naver.com>
2022-10-15 01:17:18 +0900

    Header 수정

--- src/etc/Manager_Components/Header.js
+++ src/etc/Manager_Components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import '../css/ManagerHeader.css'
 function Header() {
+  const today = new Date();
+  const mon = today.getMonth() + 1;
+
   return (
-    <div>상단</div>
+    <div className='Header'>
+      <div className='Month'>{mon}</div>
+      <div className='month_header'>월 근로 확인</div>
+    </div>
   )
 }
 
--- src/manager/page/UserApproval/UserApprovalPage.js
+++ src/manager/page/UserApproval/UserApprovalPage.js
@@ -1,16 +1,30 @@
 import React from 'react'
-import '../UserApproval/css/UserApprovalSession.css'
+import '../UserApproval/css/UserApprovalPage.css'
+import TopHeader from '../../../etc/Manager_Components/TopHeader';
+import Header from '../../../etc/Manager_Components/Header';
+import SideMenu from '../../../etc/Manager_Components/SideMenu';
+import UserApprovalPageSession from './components/UserApprovalSession';
 
 
 function UserApprovalPage() {
   return (
     <div className='UserApprovalPage'>
-        <header>
-        </header>
+      {/* 최상위 상단 */}
+      <header>
+          <TopHeader/>
+      </header>
+      <div className='userapproval_session'>
+        {/* 사이드 메뉴 */}
         <div className='SideMenu'>
+          <SideMenu/>
         </div>
-        <div className='UserApprovalSession'>
+        <div className='UserApprovalPageSession'>
+          {/* 상단 */}
+          <Header/>
+          {/* 회원 등록 영역 */}
+          <UserApprovalPageSession/>
         </div>
+      </div>
     </div>
   )
 }
