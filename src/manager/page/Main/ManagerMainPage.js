3ee66489c0accfffa96fed22715b537f60cdef89
so0y <83389222+so0y@users.noreply.github.com>
2022-12-06 02:54:32 +0900

    Edit Css Manager

--- src/manager/page/Main/ManagerMainPage.js
+++ src/manager/page/Main/ManagerMainPage.js
@@ -1,31 +1,27 @@
-import React from 'react'
-import '../Main/css/ManagerMainPage.css';
-import TopHeader from '../../../etc/Manager_Components/TopHeader';
-import Header from '../../../etc/Manager_Components/Header';
-import SideMenu from '../../../etc/Manager_Components/SideMenu';
-import ManagerSession from '../../../manager/page/Main/components/ManagerMainSession'
+import React from "react";
+import "../Main/css/ManagerMainPage.css";
+import TopHeader from "../../../etc/Manager_Components/TopHeader";
+import Header from "../../../etc/Manager_Components/Header";
+import SideMenu from "../../../etc/Manager_Components/SideMenu";
+import ManagerSession from "../../../manager/page/Main/components/ManagerMainSession";
 
 function ManagerMainPage() {
   return (
-    <div className='ManagerMainPage'>
-      {/* 최상위 상단 */}
+    <div className="ManagerMainPage">
       <header>
-          <TopHeader/>
+        <TopHeader />
       </header>
-      <div className='managermain_session'>
-        {/* 사이드 메뉴 */}
-        <div className='SideMenu'>
-          <SideMenu/>
+      <div className="managermain_session">
+        <div className="SideMenu">
+          <SideMenu />
         </div>
-        <div className='ManagerMainSession'>
-          {/* 상단 */}
-          <Header/>
-          {/* 관리자 메인 영역 */}
-          <ManagerSession/>
+        <div className="ManagerMainSession">
+          <Header />
+          <ManagerSession />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ManagerMainPage
+export default ManagerMainPage;
