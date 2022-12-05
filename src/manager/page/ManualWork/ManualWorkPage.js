3df99e52d69a67d989cf0a9ef64625d9cebefb7c
so0y <83389222+so0y@users.noreply.github.com>
2022-12-05 20:48:40 +0900

    edit

--- src/manager/page/ManualWork/ManualWorkPage.js
+++ src/manager/page/ManualWork/ManualWorkPage.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import '../ManualWork/css/ManualWorkPage.css'
-import TopHeader from '../../../etc/Manager_Components/TopHeader'
-import SideMenu from '../../../etc/Manager_Components/SideMenu'
-import Header from '../../../etc/Manager_Components/Header'
-import ManualWorkSession from './components/ManualWorkSession'
+import React from "react";
+import "../ManualWork/css/ManualWorkPage.css";
+import TopHeader from "../../../etc/Manager_Components/TopHeader";
+import SideMenu from "../../../etc/Manager_Components/SideMenu";
+import Header from "../../../etc/Manager_Components/Header";
+import ManualWorkSession from "./components/ManualWorkSession";
 
 function ManualWorkPage() {
   return (
-    <div className='ManualWorkPage'>
+    <div className="ManualWorkPage">
       {/* 최상위 상단 */}
       <header>
-          <TopHeader/>
+        <TopHeader />
       </header>
-      <div className='manualwork_session'>
+      <div className="manualwork_session">
         {/* 사이드 메뉴 */}
-        <div className='SideMenu'>
-          <SideMenu/>
+        <div className="SideMenu">
+          <SideMenu />
         </div>
-        <div className='ManualWorkSession'>
+        <div className="ManualWorkSession">
           {/* 상단 */}
-          <Header/>
+          <Header />
           {/* 관리자 메인 영역 */}
-          <ManualWorkSession/>
+          <ManualWorkSession />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ManualWorkPage
+export default ManualWorkPage;
